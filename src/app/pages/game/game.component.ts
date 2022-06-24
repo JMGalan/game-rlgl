4509128f634232d3e33e1fbc59e874b98dbac1aa
jmgalan <jmgalan@minsait.com>
2022-06-24 10:37:09 +0200

    inicio logica semaforo

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 import { AuthService } from '../../core/services/auth.service';
+import { getHtmlTagDefinition } from '@angular/compiler';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styles: []
 })
-export class GameComponent implements OnDestroy {
+export class GameComponent implements OnInit, OnDestroy {
 
   userName: string = '';
   highScore: number = 0;
   score: number = 0;
+
+  semaforoState: string = 'red';
+
 
   authStateSubs!: Subscription;
   getCollectionSubs!: Subscription;
@@ -28,8 +32,7 @@ export class GameComponent implements OnDestroy {
       if (fuser && fuser.email) {
         this.userName = fuser?.email?.split('@')[0];
 
-        this.getCollectionSubs = this.authServ.getCollection(fuser.uid)
-        .subscribe( (doc) => {
+        this.getCollectionSubs = this.authServ.getCollection(fuser.uid).subscribe( (doc) => {
           this.highScore = doc[0].highscore;
           this.score = doc[0].score;
           localStorage.setItem('docUser', JSON.stringify(doc[0]));
@@ -38,6 +41,23 @@ export class GameComponent implements OnDestroy {
     });
   }
 
+  ngOnInit() {
+    this.gameInit();
+  }
+
+  gameButtonClick(mode: string) {
+    console.log('click en ... '+mode);
+  }
+
+  gameInit() {
+    this.semaforoState = 'red';
+    let setTOgreen = setTimeout( ()=> {
+      this.semaforoState = 'green';
+    }, 3000)
+  }
+
+
+  
   logout() {
     this.authServ.logout().then( ()=> {
       this.router.navigateByUrl('');
@@ -47,9 +67,5 @@ export class GameComponent implements OnDestroy {
   ngOnDestroy() {
     this.authStateSubs.unsubscribe();
     this.getCollectionSubs.unsubscribe();
-  }
-
-  gameButtonClick(mode: string) {
-    console.log('click en ... '+mode);
   }
 }

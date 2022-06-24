2c51b16c6299418fce320427267e856e3742869e
jmgalan <jmgalan@minsait.com>
2022-06-24 10:46:19 +0200

    semaforo

--- src/app/shared/components/semaforo/semaforo.component.ts
+++ src/app/shared/components/semaforo/semaforo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-semaforo',
@@ -7,10 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class SemaforoComponent {
 
-  isBulbOn: boolean = true;
-
-  @Input() userName = '';
-  @Output() onLogout : EventEmitter<boolean> = new EventEmitter();
+  @Input() semaforoState: string = "red";
 
   constructor() {}
 
